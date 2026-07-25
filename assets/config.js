9644695c96ddf05e4ae043deb6a3c2890e9b84da
Harne123 <harnezxc@gmail.com>
2026-07-26 01:40:25 +0300

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.VELORA_SITE = {
-  "version": "1.3.1",
-  "downloadUrl": "https://github.com/Harne123/velora1/releases/latest/download/Velora-1.3.1-x64.exe",
-  "portableUrl": "https://github.com/Harne123/velora1/releases/latest/download/Velora-1.3.1-Portable.exe",
+  "version": "1.4.0",
+  "downloadUrl": "https://github.com/Harne123/velora1/releases/latest/download/Velora-1.4.0-x64.exe",
+  "portableUrl": "https://github.com/Harne123/velora1/releases/latest/download/Velora-1.4.0-Portable.exe",
   "publicUrl": "https://Harne123.github.io/velora1",
   "provider": "github"
 };
