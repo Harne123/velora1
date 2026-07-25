fecd395ac57351158f16bdef3026d7707fcd23ce
Harne123 <harnezxc@gmail.com>
2026-07-25 21:58:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.VELORA_SITE = {
+  "version": "1.1.0",
+  "downloadUrl": "./download/Velora-Setup.exe",
+  "portableUrl": "./download/Velora-Portable.exe",
+  "publicUrl": "https://ВАШ-ДОМЕН.ru"
+};
